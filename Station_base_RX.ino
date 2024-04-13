3583db1b4db8574923defe8f2740396ddc32043b
laflecherapide <laflecherapide@hotmail.com>
2024-04-13 18:02:11 +0100

    mise en place du système de communication
    retranscription de la trame originel et affichage

--- Station_base_RX.ino
+++ Station_base_RX.ino
@@ -15,14 +15,25 @@
 #define RFM69_INT 3
 #define RFM69_CS 4
 #define RFM69_RST 2
+//#define debug_reception
 
 //************* DEFINITION DES OBJETS ************
 RH_RF69 rfm69(RFM69_CS, RFM69_INT);
 
 //************* DEFINITION DES VARIABLES ************
+String tableau_reception[40];
+int compteur = 0;
+int cpt_char = 0;
 uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];  // Buffer de réception
 // FORMAT :Packetnum|Time_ms|TMP36_Temperature|BMP280_Temperature|BMP280_Pression|BMP280_AltitudeApprox|OzoneConcentration;\r\n
 uint8_t len;  // Taille du buffer de réception
+char *token;                            // Pointeur vers le token extrait
+int integerPart;
+char *token_start;
+float floatPart;
+int nb_packet;
+int len_;
+
 
 unsigned long Time_ms;  // "temps" en milliseconde depuis le dernier reset du uP
 
@@ -38,25 +49,31 @@ void setup() {
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  if (!rfm69.init()) {
+  if (!rfm69.init()) 
+  {
 
     while (1)  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ATTENTION BLOQUANT !!!!!!!!!!!!!!!!!!!!!!
     {
       Serial.println("RFM69 radio init failed");
       delay(200);
     }
-  } else {
+  } 
+  else 
+  {
     Serial.println("RFM69 radio init Succes");
   }
 
-  if (!rfm69.setFrequency(RF69_FREQ)) {
+  if (!rfm69.setFrequency(RF69_FREQ)) 
+  {
 
     while (1)  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ATTENTION BLOQUANT !!!!!!!!!!!!!!!!!!!!!!
     {
       Serial.println("setFrequency failed");
       delay(200);
     }
-  } else {
+  } 
+  else 
+  {
     Serial.println("setFrequency Succes");
   }
 
@@ -76,22 +93,57 @@ void loop() {
   {
     // Récupération et affichage des données
     len = sizeof(buf);
-    if (rfm69.recv(buf, &len)) {
+    if (rfm69.recv(buf, &len)) 
+    {
       if (!len) return;
       buf[len] = 0;
-      Serial.print("Received[");
-      Serial.print(len);
-      Serial.print("] ");
-      Serial.print("RSSI:");
-      Serial.print(rfm69.lastRssi(), DEC);
-      Serial.print(" ");
-      Serial.print((char*)buf);
-    } else {
+      token = strtok(buf, ",");              // Découper la chaîne en fonction de la virgule comme délimiteur
+      integerPart = atoi(token);        // Récupérer le nombre entier avant la virgule
+      token = strtok(NULL, ",");           // Récupérer la partie flottante après la virgule
+      floatPart = atof(token);
+      
+      if (++compteur == integerPart)
+      {
+        Serial.print(floatPart);
+        Serial.print(",");
+      }
+
+      #ifdef debug_reception
+      Serial.println((char*)buf);
+      #endif
+      if (buf[0] == '#') 
+      {
+        Serial.print("RSSI:");
+        Serial.print(rfm69.lastRssi(), DEC);
+        Serial.print(" ");
+        token_start = strtok(buf, "#");
+        nb_packet = atoi(token_start);
+        
+        for(int i; i < nb_packet+1 ;i++)
+        {         //remplir le tableau pour detecter les erreurs
+          tableau_reception[i] = String("erreur");
+        }
+        compteur = 0;
+        Serial.print("#,");
+        Serial.print(nb_packet);
+        Serial.print(", ");
+      }
+
+      if (buf[len - 1] == '$') 
+      {
+        Serial.print("$");
+        len_ = sizeof(tableau_reception);
+      }
+    } else 
+    {
       Serial.println("Receive failed");
     }
-  } else {
+  } 
+  else 
+  {
 
-    if (millis() >= Time_ms + 1500) {  //prends une mesure toute les 10ms
+    if (millis() >= Time_ms + 1500) 
+    {  //prends une mesure toute les 10ms
       Serial.println(".");
       Time_ms = millis();
     }
